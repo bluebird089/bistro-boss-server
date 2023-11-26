a8af4337724bef5e7f870623345a8ee5ff6650fe
Blue Bird <bluebird089@proton.me>
2023-11-27 02:18:18 +0600

    added code to add jwt and verify it

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
 
 // Middlewares
 app.use(cors());
@@ -33,6 +33,14 @@ async function run() {
             .collection("reviews");
         const cartCollection = client.db("bistroBossDB").collection("cart");
         const userCollection = client.db("bistroBossDB").collection("user");
+
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                expiresIn: "1h",
+            });
+            res.send({ token });
+        });
 
         app.get("/menu", async (req, res) => {
             const result = await menuCollection.find().toArray();
@@ -81,7 +89,25 @@ async function run() {
             res.send(result);
         });
 
-        app.get("/users", async (req, res) => {
+        // Middlewares
+        const verifyToken = (req, res, next) => {
+            console.log(req.headers);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "Forbidden Access" });
+            }
+            const token = req.headers.authorization.split(" ")[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+                if (err) {
+                    return res
+                        .status(401)
+                        .send({ message: "Forbidden Access" });
+                }
+                req.decoded = decoded;
+                next();
+            });
+        };
+
+        app.get("/users", verifyToken, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         });
