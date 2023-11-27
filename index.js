011fa25a963c6f7e345cd7ded27001764321f08e
Blue Bird <bluebird089@proton.me>
2023-11-27 12:13:33 +0600

    added isAdmin middleware

--- index.js
+++ index.js
@@ -107,7 +107,19 @@ async function run() {
             });
         };
 
-        app.get("/users", verifyToken, async (req, res) => {
+        // Verify Admin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === "admin";
+            if (isAdmin) {
+                return res.status(403).send({ message: "Forbidden Access" });
+            }
+            next();
+        };
+
+        app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         });
@@ -126,24 +138,32 @@ async function run() {
             res.send({ admin });
         });
 
-        app.delete("/users/:id", async (req, res) => {
+        app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await userCollection.deleteOne(query);
             res.send(result);
         });
 
-        app.patch("/users/admin/:id", async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
-            const updatedDoc = {
-                $set: {
-                    role: "admin",
-                },
-            };
-            const result = await userCollection.updateOne(filter, updatedDoc);
-            res.send(result);
-        });
+        app.patch(
+            "/users/admin/:id",
+            verifyToken,
+            verifyAdmin,
+            async (req, res) => {
+                const id = req.params.id;
+                const filter = { _id: new ObjectId(id) };
+                const updatedDoc = {
+                    $set: {
+                        role: "admin",
+                    },
+                };
+                const result = await userCollection.updateOne(
+                    filter,
+                    updatedDoc
+                );
+                res.send(result);
+            }
+        );
 
         await client.db("admin").command({ ping: 1 });
         console.log(
