434bd0250701bb563ccb73f8ff66f737541ea1b9
Blue Bird <bluebird089@proton.me>
2023-11-29 03:53:47 +0600

    added code to make purchase via stripe

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-var jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // Middlewares
 app.use(cors());
@@ -111,6 +112,21 @@ async function run() {
             const query = { _id: new ObjectId(id) };
             const result = await cartCollection.deleteOne(query);
             res.send(result);
+        });
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
         });
 
         app.post("/cart", async (req, res) => {
